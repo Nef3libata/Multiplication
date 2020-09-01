9f46483d9596c6966e1dd5eb6205f83810d25d4a
Nef3libata <samanehcosmonaut@gmail.com>
2020-09-01 19:21:06 +0430

    first commit

--- app/src/main/java/com/example/multiplication/MainActivity.java
+++ app/src/main/java/com/example/multiplication/MainActivity.java
@@ -27,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     EditText answerText;
     ImageView imageView;
     Random r;
+    int answer;
 
     Integer[] images = {R.drawable.img1, R.drawable.img2, R.drawable.img3, R.drawable.img4, R.drawable.img5, R.drawable.img6, R.drawable.img7, R.drawable.img8, R.drawable.img8, R.drawable.img9, R.drawable.img10, R.drawable.img11, R.drawable.img12, R.drawable.img13, R.drawable.img14, R.drawable.img15, R.drawable.img16, R.drawable.img17, R.drawable.img18, R.drawable.img19, R.drawable.img20, R.drawable.img21, R.drawable.img22, R.drawable.img21, R.drawable.img23, R.drawable.img24, R.drawable.img25, R.drawable.img26, R.drawable.img27, R.drawable.img28, R.drawable.img29, R.drawable.img30, R.drawable.img31, R.drawable.img32, R.drawable.img33, R.drawable.img34, R.drawable.img35, R.drawable.img36, R.drawable.img37, R.drawable.img38, R.drawable.img39, R.drawable.img40, R.drawable.img41, R.drawable.img42, R.drawable.img43, R.drawable.img44, R.drawable.img45, R.drawable.img46, R.drawable.img47};
 
@@ -35,7 +36,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.newBtn).setOnClickListener(buttonClickListener);
         findViewById(R.id.check).setOnClickListener(buttonClickListener);
         generateRandomM();
     }
@@ -54,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
     public void generateRandomM(){
 
         Random random = new Random();
-        int max = 10;
+        int max = 9;
         int min = 2;
         TextView randomText = (TextView) findViewById(R.id.randomText);
         int num1 = random.nextInt(max - min + 1) + min;
@@ -81,35 +81,37 @@ public class MainActivity extends AppCompatActivity {
                     String result = null;
                     answerText = (EditText) findViewById(R.id.answer);
                     resultTextView = (TextView) findViewById(R.id.result);
+                    imageView = (ImageView) findViewById(R.id.img);
 
                     r = new Random();
 
-                    int answer = Integer.parseInt(answerText.getText().toString());
+                    if (TextUtils.isEmpty(answerText.getText().toString())) {
+                        answer = 0;
+                    }else {
+                        answer = Integer.parseInt(answerText.getText().toString());
+                    }
 
                     if (wordStr == answer) {
                         result = rnBravo();
-                        imageView = (ImageView) findViewById(R.id.img);
+
                         imageView.setImageResource(images[r.nextInt(images.length)]);
+                        resultTextView.setText("");
+                        answerText.setText("");
+
+                        generateRandomM();
 
                     } else if(wordStr != answer){
+
+                        imageView.setImageBitmap(null);
                         result = "بیشتر دقت کن! جواب درست:" + " " + wordStr;
-                    } else if (TextUtils.isEmpty(answerText.getText().toString())) {
+
+
+                    } else if (answer == 0) {
                         result = "هنوز جواب ندادی که!";
                     }
 
                     resultTextView.setText(result + " ");
                     break;
-
-                case R.id.newBtn:
-
-                    resultTextView.setText("");
-                    answerText.setText("");
-                    imageView.setImageBitmap(null);
-
-                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-                    imm.toggleSoftInput(InputMethodManager.SHOW_IMPLICIT,0);
-
-                    generateRandomM();
 
             }
         }
